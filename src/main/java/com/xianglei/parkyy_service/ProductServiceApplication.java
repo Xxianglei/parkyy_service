9758f07898811d05dc6c0eba34407a730341d888
BB  zebra <1767925069@qq.com>
2019-11-25 23:36:19 +0800

    Update ProductServiceApplication.java

--- src/main/java/com/xianglei/parkyy_service/ProductServiceApplication.java
+++ src/main/java/com/xianglei/parkyy_service/ProductServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class ProductServiceApplication extends SpringBootServletInitializer {
 
     @Override
